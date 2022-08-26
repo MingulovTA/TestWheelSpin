539cbbd413402870b9057187285c1f77199fc465
DrTressi <DrTressi@yandex.ru>
2022-08-27 06:16:19 +0700

    feat: aspect ratio support added

--- Assets/TestWheelSpin/Core/ApplicationStartup.cs
+++ Assets/TestWheelSpin/Core/ApplicationStartup.cs
@@ -9,13 +9,37 @@ namespace TestWheelSpin.Core
     {
         [SerializeField] private PopupGameplay _wheelGameplay;
 
+        private ScreenOrientation _currentScreenOrientation;
         private void Start()
         {
+            _currentScreenOrientation = Screen.orientation;
+            RebuildScreenOrientation();
             _wheelGameplay.HideMomentary();
         }
         private void OnMouseDown()
         {
             _wheelGameplay.Show();
         }
+
+        private void Update()
+        {
+            if (_currentScreenOrientation != Screen.orientation)
+                RebuildScreenOrientation();
+        }
+
+        private void RebuildScreenOrientation()
+        {
+            _currentScreenOrientation = Screen.orientation;
+            float aspectRation = (float)Screen.width / Screen.height;
+            if ( aspectRation < 1)
+            {
+                _wheelGameplay.transform.localScale = Vector3.one * aspectRation;
+            }
+            else
+            {
+                _wheelGameplay.transform.localScale = Vector3.one;
+            }
+        }
+        
     }
 }
